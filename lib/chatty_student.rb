a409fba81ae67f8a9968dacf015d19e246841568
steve <stevepesce879@gmail.com>
2020-06-20 19:38:59 -0400

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,10 @@
+class ChattyStudent < Student
+  def hello
+    super()
+    str = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    puts str
+  end
+  def raise_hand
+    10.times{super()}
+  end
+end
